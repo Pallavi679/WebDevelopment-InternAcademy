45261644a7fc1f58622a67451f1e4515bc1a23a9
Pallavi679 <pallavi.pandey_19@sakec.ac.in>
2021-09-23 20:10:25 +0530

    home page completed and courses page is also added

---
+++ footer.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>E-learning</title>
+    <!-- google font cnd link -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600&display=swap" rel="stylesheet">
+
+    <!-- font-awesome cdn link -->
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
+        integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
+    <!-- bootstrap cdn link -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css">
+    <!-- custom css file link -->
+    <link rel="stylesheet" href="./Css/style.css">
+</head>
+<body>
+<div class="footer">
+        <div class="box-container">
+            <div class="box">
+                <h3>branch locations</h3>
+                <a href="#">India</a>
+                <a href="#">USA</a>
+                <a href="#">france</a>
+                <a href="#">russia</a>
+            </div>
+            <div class="box">
+                <h3>quick links</h3>
+                <a href="#">home</a>
+                <a href="#">about us</a>
+                <a href="#">courses</a>
+                <a href="#">teachers</a>
+                <a href="#">contact us</a>
+            </div>
+            <div class="box">
+                <h3>contact info</h3>
+                <p><i class="fas fa-map-marker-alt"></i>navi mumbai, india 400701</p>
+                <p><i class="fas fa-envelope"></i>example.com</p>
+                <p><i class="fas fa-phone"></i>+123-12136556</p>
+            </div>
+        </div>
+        <h1 class="credit">created by WebDevp Team | all rights reserved</h1>
+    </div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,225 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>E-learning</title>
+   
+
+</head>
+
+<body>
+   <?php include 'navbar.php'?>
+    <!-- login form -->
+    <div class="login-form">
+        <form action="">
+            <h2>Login</h2>
+            <input type="email" placeholder="username" class="box">
+            <input type="password" placeholder="password" class="box">
+            <p>forget password? <a href="#">click here</a></p>
+            <p>don't have an account? <a href="#">register now</a></p>
+            <input type="submit" class="btn" value="login">
+            <i class="fas fa-times"></i>
+        </form>
+    </div>
+    <!-- home section starts  -->
+    <section class="home" id="home">
+        <h1>education from home</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat blanditiis aspernatur voluptatum. Deleniti
+            facilis voluptatibus ducimus, non, totam quibusdam illum, tempora commodi sequi eius a.</p>
+        <a href="#"><button class="btn">get started</button></a>
+        <div class="shape"></div>
+    </section>
+    <!-- home section ends -->
+    <!-- about section starts -->
+    <section class="about" id="about">
+        <div class="image">
+            <img src="./Img/Studying-rafiki.svg" alt="">
+        </div>
+        <div class="content">
+            <h3>Why choose us?</h3>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptates maxime saepe eum.Lorem ipsum dolor
+                sit, amet consectetur adipisicing elit. Voluptates maxime saepe eum</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptates maxime saepe eum. Lorem ipsum dolor
+                sit amet consectetur adipisicing elit. Hic, necessitatibus minus distinctio libero placeat veritatis,
+                similique voluptates adipisci rem sit nobis, nemo veniam? Amet, assumenda.</p>
+            <a href="#"><button class="btn">learn more</button></a>
+        </div>
+    </section>
+    <!-- about section end -->
+    <!-- course section starts -->
+    <section class="heading course">
+        <h1 class="heading">our popular courses</h1>
+        <div class="box-container">
+            <div class="box">
+                <img src="./Img/3426526.jpg" alt="">
+                <h3 class="price">5000/-</h3>
+                <div class="content">
+                    <div class="stars">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star-half"></i>
+                    </div>
+
+                </div>
+                <a href="#" class="title">learn front end development</a>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde vitae ratione, ab quod eos quisquam
+                    quas itaque ullam sunt ex? Optio eaque autem ipsa velit.</p>
+                <div class="info">
+                    <h3><i class="far fa-clock"></i>2 hours</h3>
+                    <h3><i class="far fa-calendar-alt"></i>6 months</h3>
+                    <h3><i class="far fa-book"></i>12 modules</h3>
+                </div>
+            </div>
+            <div class="box">
+                <img src="./Img/3426526.jpg" alt="">
+                <h3 class="price">5000/-</h3>
+                <div class="content">
+                    <div class="stars">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star-half"></i>
+                    </div>
+
+                </div>
+                <a href="#" class="title">learn front end development</a>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde vitae ratione, ab quod eos quisquam
+                    quas itaque ullam sunt ex? Optio eaque autem ipsa velit.</p>
+                <div class="info">
+                    <h3><i class="far fa-clock"></i>2 hours</h3>
+                    <h3><i class="far fa-calendar-alt"></i>6 months</h3>
+                    <h3><i class="far fa-book"></i>12 modules</h3>
+                </div>
+            </div>
+            <div class="box">
+                <img src="./Img/3426526.jpg" alt="">
+                <h3 class="price">5000/-</h3>
+                <div class="content">
+                    <div class="stars">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star-half"></i>
+                    </div>
+
+                </div>
+                <a href="#" class="title">learn front end development</a>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde vitae ratione, ab quod eos quisquam
+                    quas itaque ullam sunt ex? Optio eaque autem ipsa velit.</p>
+                <div class="info">
+                    <h3><i class="far fa-clock"></i>2 hours</h3>
+                    <h3><i class="far fa-calendar-alt"></i>6 months</h3>
+                    <h3><i class="far fa-book"></i>12 modules</h3>
+                </div>
+            </div>
+            <div class="box">
+                <img src="./Img/3426526.jpg" alt="">
+                <h3 class="price">5000/-</h3>
+                <div class="content">
+                    <div class="stars">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star-half"></i>
+                    </div>
+
+                </div>
+                <a href="#" class="title">learn front end development</a>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde vitae ratione, ab quod eos quisquam
+                    quas itaque ullam sunt ex? Optio eaque autem ipsa velit.</p>
+                <div class="info">
+                    <h3><i class="far fa-clock"></i>2 hours</h3>
+                    <h3><i class="far fa-calendar-alt"></i>6 months</h3>
+                    <h3><i class="far fa-book"></i>12 modules</h3>
+                </div>
+            </div>
+            <div class="box">
+                <img src="./Img/3426526.jpg" alt="">
+                <h3 class="price">5000/-</h3>
+                <div class="content">
+                    <div class="stars">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star-half"></i>
+                    </div>
+
+                </div>
+                <a href="#" class="title">learn front end development</a>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde vitae ratione, ab quod eos quisquam
+                    quas itaque ullam sunt ex? Optio eaque autem ipsa velit.</p>
+                <div class="info">
+                    <h3><i class="far fa-clock"></i>2 hours</h3>
+                    <h3><i class="far fa-calendar-alt"></i>6 months</h3>
+                    <h3><i class="far fa-book"></i>12 modules</h3>
+                </div>
+            </div>
+            <div class="box">
+                <img src="./Img/3426526.jpg" alt="">
+                <h3 class="price">5000/-</h3>
+                <div class="content">
+                    <div class="stars">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star-half"></i>
+                    </div>
+
+                </div>
+                <a href="#" class="title">learn front end development</a>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde vitae ratione, ab quod eos quisquam
+                    quas itaque ullam sunt ex? Optio eaque autem ipsa velit.</p>
+                <div class="info">
+                    <h3><i class="far fa-clock"></i>2 hours</h3>
+                    <h3><i class="far fa-calendar-alt"></i>6 months</h3>
+                    <h3><i class="far fa-book"></i>12 modules</h3>
+                </div>
+            </div>
+    </section>
+    <!-- course section ends -->
+    <!-- teacher section start -->
+    <section class="teacher" id="teacher">
+        <h1 class="heading">Our expert teachers</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, labore, accusantium illum hic sint magni
+            delectus illo sapiente doloribus blanditiis distinctio recusandae pariatur nobis quod!</p>
+        <a href="#"><button class="btn">learn more</button></a>
+    </section>
+    <!-- teacher section end -->
+    <!-- contact us section start -->
+    <section class="contact" id="contact">
+        <h1 class="heading">contact us</h1>
+        <div class="row">
+            <form action="#">
+                <input type="text" placeholder="full name" class='box'>
+                <input type="email" placeholder="your email" class='box'>
+                <input type="number" placeholder="your number" class='box'>
+                <textarea name="" id="" cols="30" rows="10" placeholder="your message" class="box address"></textarea>
+                <input type="submit" class="btn" value="send now">
+            </form>
+            <div class="image">
+                <img src="./Img/contact.png" alt="">
+            </div>
+        </div>
+    </section>
+    <!-- contact us section end -->
+    <!-- footer section start -->
+  <?php include 'footer.php'?>
+    
+    <!-- footer section end -->
+</body>
+<!-- jquery cdn link -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+<!-- custom script link -->
+<script src="./Script/script.js"></script>
+
+</html>
